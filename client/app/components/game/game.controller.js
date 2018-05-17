9d7a851266e7cd743e927eabf8ba881accc2c8f0
Miguel Botello <miguel.botello@globant.com>
2018-05-17 09:37:19 -0500

    - Initialize the custom seeds in the grid - Adding comments - Improve colors to grid and cells selecteds

--- client/app/components/game/game.controller.js
+++ client/app/components/game/game.controller.js
@@ -15,24 +15,56 @@ class GameController {
     this._initializeGrid();
   }
 
-  _initializeCells() {
+  /**
+   * Validate if the cell with the position x and y is a custom seed
+   * @param {number} indexRow
+   * @param {number} indexCol
+   * @return {number}
+   * @private
+   */
+  _isCustomSeed(indexRow, indexCol) {
+    let seedFlag = 0;
+    if ((indexRow === 0 && indexCol === 0) ||
+      (indexRow === 0 && indexCol === 1) ||
+      (indexRow === 1 && indexCol === 0) ||
+      (indexRow === 1 && indexCol === 3) ||
+      (indexRow === 2 && indexCol === 1) ||
+      (indexRow === 2 && indexCol === 2)) {
+      seedFlag = 1;
+    }
+
+    return seedFlag;
+  };
+
+  /**
+   * Initialize the cells taking account the custom seeds
+   * @param {number} indexRow
+   * @return {Array}
+   * @private
+   */
+  _initializeCells(indexRow) {
     const cells = [];
 
     // Generate cells
     for (let indexCol = 0; indexCol < this.constants.MAX_COL; indexCol += 1) {
-      const cell = { active: 0 };
+      const cell = { active: this._isCustomSeed(indexRow, indexCol) };
       cells.push(cell);
     }
 
     return cells;
   }
 
+  /**
+   * Initialize the rows with cells
+   * @return {Array}
+   * @private
+   */
   _initializeRows() {
     const rows = [];
 
     // Generate the rows
     for (let indexRow = 0; indexRow < this.constants.MAX_ROW; indexRow += 1) {
-      const cols = this._initializeCells();
+      const cols = this._initializeCells(indexRow);
       rows.push(cols);
     }
 
